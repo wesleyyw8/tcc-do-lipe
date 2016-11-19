2f9d330dd2aa5f38faa2ecdfa69d750ae95d0d41
Wesley Rebelo <wesleyyw8@gmail.com>
2016-11-19 17:23:19 -0200

    adawdawd

--- routes/buscar.js
+++ routes/buscar.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-router.route('/:mat')
+router.route('/:mat/:classe/:tipo/:amb/:porte')
   .get(function(request,response){
     getConnection(function(error, con){
       if(error) throw error;
-      var query = "SELECT NOME_BRINC, APLICACAO_BRINC from BRINCADEIRA WHERE MAT = '"+request.params.mat+"'";
+      var query = 'SELECT NOME_BRINC, APLICACAO_BRINC from BRINCADEIRA WHERE ';
+      if (request.params.mat != 'null')
+        query += ' MAT = "'+request.params.mat+'"';
+
+      console.log(query);
+      if (request.params.classe != 'null')
+        query += ' CLASS = '+request.params.classe;
+      if (request.params.tipo != 'null')
+        query += ' TIPO = '+request.params.tipo;
+      if (request.params.amb != 'null')
+        query += ' AMB = '+request.params.amb;
+      if (request.params.porte != 'null')
+        query += ' PORTE = '+request.params.porte;
+
+      //WHERE MAT = 'Aritmética' and CLASS= 'Primario' and TIPO = 'Tabuleiro' and AMB = 'Interno' and porte= 'Pequeno';
       con.query(query,function(err,rows){
         if(err) throw err;
         console.log('Data received from Db:\n');
